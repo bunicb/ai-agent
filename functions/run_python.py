7de1a01baee66e8f98864fc2b2aa8aad83d0d61b
Bojan Bunić <bojan@bunic.info>
2025-07-06 09:42:02 +0200

    add run python

---
+++ functions/run_python.py
@@ -0,0 +1,35 @@
+import os, subprocess
+
+def run_python_file(working_directory, file_path):
+    work_dir = os.path.abspath(working_directory)
+    target_file = work_dir
+    if file_path:
+        target_file = os.path.abspath(os.path.join(working_directory, file_path))
+    if not target_file.startswith(work_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(target_file):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        commands = ["python", target_file]
+        result = subprocess.run(commands,
+            timeout=30,
+            capture_output=True,
+            text=True,
+            cwd=work_dir
+        )
+
+        output = []
+        if result.stdout:
+            output.append(f"STDOUT:\n{result.stdout}")
+        if result.stderr:
+            output.append(f"STDERR:\n{result.stderr}")
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+
+        return "\n".join(output) if output else "No output produced."
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
